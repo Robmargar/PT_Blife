68bf9c0405f786aaa0694e65600b3c5cf265dc57
Robmargar <roberto.mar.garc@gmail.com>
2024-04-10 23:06:48 -0600

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/Robmargar/PT_Blife",
+  base: "/PT_Blife/",
   plugins: [react()],
-})
+});
